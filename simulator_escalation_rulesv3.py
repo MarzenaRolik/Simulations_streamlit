6efe8cfd03f0a3cb027501445e5f3f5eedef0cba
MarzenaRolik <marzena.rolik@gmail.com>
2024-12-20 15:58:36 +0100

    v3

--- simulator_escalation_rulesv3.py
+++ simulator_escalation_rulesv3.py
@@ -14,19 +14,23 @@ def load_default_thresholds():
         # Default thresholds if no file exists
         return {
             "DEFAULT": {
-                "vol_l4": 500000.01,
-                "vol_l3": 120000.01,
-                "vol_l2": 80000.01,
-                "vol_l1": 50000.01,
-                "gm_l4": 67,
-                "gm_l3": 70,
-                "gm_l2": 74,
-                "gm_l1": 77,
-                "ds_l5": 10,
-                "ds_l4": 21,
-                "ds_l3": 30,
-                "ds_l2": 55,
-                "ds_l1": 81
+                "vol_l4": 10000000.01,
+                "vol_l3": 500000.01,
+                "vol_l2": 120000.01,
+                "vol_l1" : 80000.01,
+                "vol_l0" : 50000.01,
+                
+                "gm_l4" : 0,
+                "gm_l3" : 67,
+                "gm_l2" : 70,
+                "gm_l1" : 74,
+                "gm_l0" : 77,
+
+                "ds_l4" : 10,
+                "ds_l3" : 21,
+                "ds_l2" : 30,
+                "ds_l1" : 55,
+                "ds_l0" : 81
             }
         }
 
@@ -37,44 +41,42 @@ def save_thresholds(thresholds):
 
 def determine_vol_level(vol, thresholds):
     if vol > thresholds['vol_l4']:
-        return 'L4'
+        return 'L5'
     elif vol > thresholds['vol_l3']:
-        return 'L3'
+        return 'L4'
     elif vol > thresholds['vol_l2']:
-        return 'L2'
+        return 'L3'
     elif vol > thresholds['vol_l1']:
+        return 'L2'
+    elif  vol > thresholds['vol_l0']:
         return 'L1'
-    elif vol > 0:
-        return 'L0'
-    return 'N/A'
+    return 'L0'
 
 def determine_gm_level(gm, thresholds):
-    if gm < thresholds['gm_l4']:
-        return 'L4'
-    elif gm < thresholds['gm_l3']:
-        return 'L3'
-    elif gm < thresholds['gm_l2']:
-        return 'L2'
-    elif gm < thresholds['gm_l1']:
-        return 'L1'
-    elif gm < 100:
+    if gm > thresholds['gm_l0']:
         return 'L0'
-    return 'N/A'
+    elif gm >  thresholds['gm_l1']:
+        return 'L1'
+    elif gm >  thresholds['gm_l2']:
+        return 'L2'
+    elif gm >  thresholds['gm_l3']:
+        return 'L3'
+    elif gm >  thresholds['gm_l4']:
+        return 'L4'
+    return 'L5'
 
 def determine_ds_level(ds, thresholds):
-    if ds < thresholds['ds_l5']:
-        return 'L5'
-    elif ds < thresholds['ds_l4']:
-        return 'L4'
-    elif ds < thresholds['ds_l3']:
-        return 'L3'
-    elif ds < thresholds['ds_l2']:
-        return 'L2'
-    elif ds < thresholds['ds_l1']:
-        return 'L1'
-    elif ds <= 100:
+    if ds > thresholds['ds_l0']:
         return 'L0'
-    return 'N/A'
+    elif ds > thresholds['ds_l1']:
+        return 'L1'
+    elif ds > thresholds['ds_l2']:
+        return 'L2'
+    elif ds > thresholds['ds_l3']:
+        return 'L3'
+    elif ds > thresholds['ds_l4']:
+        return 'L4'
+    return 'L5'
 
 def get_level_value(level):
     level_map = {'L0': 0, 'L1': 1, 'L2': 2, 'L3': 3, 'L4': 4, 'L5': 5, 'N/A': -1}
@@ -82,43 +84,45 @@ def get_level_value(level):
 
 def determine_highest_level(row, thresholds):
     levels = [
-        ('DS', get_level_value(determine_ds_level(row['DS'], thresholds))),
-        ('GM', get_level_value(determine_gm_level(row['GM'], thresholds))),
-        ('VOL', get_level_value(determine_vol_level(row['Vol'], thresholds))),
-        ('OBR', get_level_value(row['oBR_level']))
+        ('Deal Score', get_level_value(determine_ds_level(row['DS'], thresholds))),
+        ('Gross Margin %', get_level_value(determine_gm_level(row['GM'], thresholds))),
+        ('Deal size (€)', get_level_value(determine_vol_level(row['Vol'], thresholds))),
+        ('Other Bussiness Rule', get_level_value(row['oBR_level']))
     ]
     max_level = max(levels, key=lambda x: x[1])
     if max_level[0] == 'OBR' and row['FP'] ==True:
-        return 'FP'
+        return 'Below Floor Price'
     return max_level[0]
 
 def display_threshold_controls(thresholds, country):
     """Display and handle threshold controls for a specific country."""
     modified = False
     
-    st.sidebar.subheader('Volume Thresholds')
-    new_vol_l4 = st.sidebar.number_input('VOL L4 Threshold', value=float(thresholds['vol_l4']), key=f'vol_l4_{country}')
-    new_vol_l3 = st.sidebar.number_input('VOL L3 Threshold', value=float(thresholds['vol_l3']), key=f'vol_l3_{country}')
-    new_vol_l2 = st.sidebar.number_input('VOL L2 Threshold', value=float(thresholds['vol_l2']), key=f'vol_l2_{country}')
-    new_vol_l1 = st.sidebar.number_input('VOL L1 Threshold', value=float(thresholds['vol_l1']), key=f'vol_l1_{country}')
+    st.sidebar.subheader('Gross Margin % Thresholds')
+    new_gm_l0 = st.sidebar.slider('Gross Margin % L0 More Than', 0, 100, int(thresholds['gm_l0']), key=f'gm_l0_{country}')
+    new_gm_l1 = st.sidebar.slider('Gross Margin % L1 More Than', 0, 100, int(thresholds['gm_l1']), key=f'gm_l1_{country}')
+    new_gm_l2 = st.sidebar.slider('Gross Margin % L2 More Than', 0, 100, int(thresholds['gm_l2']), key=f'gm_l2_{country}')
+    new_gm_l3 = st.sidebar.slider('Gross Margin % L3 More Than', 0, 100, int(thresholds['gm_l3']), key=f'gm_l3_{country}')
+    new_gm_l4 = st.sidebar.slider('Gross Margin % L4 More Than', 0, 100, int(thresholds['gm_l4']), key=f'gm_l4_{country}')
+
+    st.sidebar.subheader('Deal Score Thresholds')
+    new_ds_l0 = st.sidebar.slider('Deal Score L0 More Than', 0, 100, int(thresholds['ds_l0']), key=f'ds_l0_{country}')
+    new_ds_l1 = st.sidebar.slider('Deal Score L1 More Than', 0, 100, int(thresholds['ds_l1']), key=f'ds_l1_{country}')
+    new_ds_l2 = st.sidebar.slider('Deal Score L2 More Than', 0, 100, int(thresholds['ds_l2']), key=f'ds_l2_{country}')
+    new_ds_l3 = st.sidebar.slider('Deal Score L3 More Than', 0, 100, int(thresholds['ds_l3']), key=f'ds_l3_{country}')
+    new_ds_l4 = st.sidebar.slider('Deal Score L4 More Than', 0, 100, int(thresholds['ds_l4']), key=f'ds_l4_{country}')
     
-    st.sidebar.subheader('GM Thresholds')
-    new_gm_l4 = st.sidebar.slider('GM L4 Threshold', 0, 100, int(thresholds['gm_l4']), key=f'gm_l4_{country}')
-    new_gm_l3 = st.sidebar.slider('GM L3 Threshold', 0, 100, int(thresholds['gm_l3']), key=f'gm_l3_{country}')
-    new_gm_l2 = st.sidebar.slider('GM L2 Threshold', 0, 100, int(thresholds['gm_l2']), key=f'gm_l2_{country}')
-    new_gm_l1 = st.sidebar.slider('GM L1 Threshold', 0, 100, int(thresholds['gm_l1']), key=f'gm_l1_{country}')
-    
-    st.sidebar.subheader('DS Thresholds')
-    new_ds_l5 = st.sidebar.slider('DS L5 Threshold', 0, 100, int(thresholds['ds_l5']), key=f'ds_l5_{country}')
-    new_ds_l4 = st.sidebar.slider('DS L4 Threshold', 0, 100, int(thresholds['ds_l4']), key=f'ds_l4_{country}')
-    new_ds_l3 = st.sidebar.slider('DS L3 Threshold', 0, 100, int(thresholds['ds_l3']), key=f'ds_l3_{country}')
-    new_ds_l2 = st.sidebar.slider('DS L2 Threshold', 0, 100, int(thresholds['ds_l2']), key=f'ds_l2_{country}')
-    new_ds_l1 = st.sidebar.slider('DS L1 Threshold', 0, 100, int(thresholds['ds_l1']), key=f'ds_l1_{country}')
-    
+    st.sidebar.subheader('Deal Size (€) Thresholds')
+    new_vol_l0 = st.sidebar.number_input('Deal Size (€) L0 Less Than', value=float(thresholds['vol_l0']), key=f'vol_l0_{country}')
+    new_vol_l1 = st.sidebar.number_input('Deal Size (€) L1 Less Than', value=float(thresholds['vol_l1']), key=f'vol_l1_{country}')
+    new_vol_l2 = st.sidebar.number_input('Deal Size (€) L2 Less Than', value=float(thresholds['vol_l2']), key=f'vol_l2_{country}')
+    new_vol_l3 = st.sidebar.number_input('Deal Size (€) L3 Less Than', value=float(thresholds['vol_l3']), key=f'vol_l3_{country}')
+    new_vol_l4 = st.sidebar.number_input('Deal Size (€) L4 Less Than', value=float(thresholds['vol_l4']), key=f'vol_l4_{country}')
+
     new_thresholds = {
-        'vol_l4': new_vol_l4, 'vol_l3': new_vol_l3, 'vol_l2': new_vol_l2, 'vol_l1': new_vol_l1,
-        'gm_l4': new_gm_l4, 'gm_l3': new_gm_l3, 'gm_l2': new_gm_l2, 'gm_l1': new_gm_l1,
-        'ds_l5': new_ds_l5, 'ds_l4': new_ds_l4, 'ds_l3': new_ds_l3, 'ds_l2': new_ds_l2, 'ds_l1': new_ds_l1
+        'vol_l4': new_vol_l4, 'vol_l3': new_vol_l3, 'vol_l2': new_vol_l2, 'vol_l1': new_vol_l1,'vol_l0': new_vol_l0,
+        'gm_l4': new_gm_l4, 'gm_l3': new_gm_l3, 'gm_l2': new_gm_l2, 'gm_l1': new_gm_l1,'gm_l0': new_gm_l0,
+         'ds_l4': new_ds_l4, 'ds_l3': new_ds_l3, 'ds_l2': new_ds_l2, 'ds_l1': new_ds_l1, 'ds_l0': new_ds_l0
     }
     
     if new_thresholds != thresholds:
